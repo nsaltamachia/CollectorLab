c6e91be39d169adb0c0d0a31f60f50d1c69b7b7a
NickSaltamachia <nsaltamachia@gmail.com>
2023-11-15 19:48:07 -0600

    create index and about pages, create Car model, add data to car model

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
+
 # Create your models here.
+class Car(models.Model):
+    model = models.CharField(max_length=100)
+    make = models.CharField(max_length=100)
+    year = models.IntegerField()
+    engine = models.CharField(max_length=1100)
+
+    def __str__(self):
+        return f'{self.make} {self.model}'
+    
--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from .models import Car
 
 # Create your views here.
+def home(request):
+    return render(request, 'home.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def cars_index(request):
+    cars = Car.objects.all()
+    return render(request, 'cars/index.html', {'cars': cars})
