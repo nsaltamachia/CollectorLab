27fac2e9ba15b8b80519f6e18e7a9246950e5bec
NickSaltamachia <nsaltamachia@gmail.com>
2023-11-17 13:53:09 -0600

    add Field Choices, move on to CRUDing with them

--- main_app/models.py
+++ main_app/models.py
@@ -3,6 +3,14 @@ from django.urls import reverse
 
 
 # Create your models here.
+
+TRAN = (
+    ('2A', '2spd automatic'),
+    ('3A', '3spd automatic'),
+    ('3M', '3spd manual'),
+    ('4M', '4spd manual')
+)
+
 class Car(models.Model):
     model = models.CharField(max_length=100)
     make = models.CharField(max_length=100)
@@ -14,4 +22,17 @@ class Car(models.Model):
     
     def get_absolute_url(self):
         return reverse('detail', kwargs={'car_id': self.id})
+    
+class Transmission(models.Model):
+    tran = models.CharField(
+        max_length=2,
+        choices=TRAN,
+        default=TRAN[1][0]
+        )
+    car = models.ForeignKey(Car, on_delete=models.CASCADE)
+    
+
+    def __str__(self):
+        return f"This beauty runs with a {self.get_tran_display()}"
+    
     
